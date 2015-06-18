2efa24bb2bee9d7de6860a3952d4c41324796914
brisk <bskblake@gmail.com>
2015-06-18 15:20:07 +0930

    Segregates functionality into functions.

--- particle.c
+++ particle.c
@@ -26,18 +26,18 @@ struct Particle {
 	float y;
 	float vx;
 	float vy;
+	float ax;
+	float ay;
 	//Time(ms) until destruction
 	int life;
 } particle;
 
-void
-quit(int exit_code) {
+void quit(int exit_code) {
 	SDL_Quit();
 	exit(exit_code);
 }
 
-void
-init() {
+void init() {
 	//Check SDL Version
 	SDL_version compiled;
 	SDL_version linked;
@@ -63,13 +63,66 @@ init() {
 	
 }
 
-int
-draw1(struct Particle particle, SDL_Texture *tex, SDL_Renderer *renderer) {
+int particle_draw(struct Particle particle, SDL_Texture *tex, SDL_Renderer *renderer) {
 	int life = particle.life;
-	float size = life < 0.1*BASE_LIFE ? life*BASE_SIZE/(0.1*BASE_LIFE): BASE_SIZE;
-	//filledCircleRGBA(renderer, particles[i].x, particles[i].y, size, 0, 0, 0, 255);
-	SDL_RenderCopy(renderer, tex, NULL, &(SDL_Rect){particle.x - size, particle.y - size, size*2, size*2});
+	float size = 8+(life < 0.1*BASE_LIFE ? life*BASE_SIZE/(0.1*BASE_LIFE): BASE_SIZE);
+	SDL_RenderCopy(renderer, tex, NULL, &(SDL_Rect){particle.x - size/2, particle.y - size/2, size, size});
 	return 1;
+}
+
+int draw2(struct Particle particle, SDL_Texture *tex, SDL_Renderer *renderer) {
+	int life = particle.life;
+	int r, g, b;
+	r = life > BASE_LIFE? 255 : life*(255/BASE_LIFE);
+	if(life < 0.25*BASE_LIFE) {
+		r = life*255/(0.25*BASE_LIFE);
+	} else {
+		r = 255;
+	}
+	if(life < 0.25*BASE_LIFE) {
+		g = 0;
+	} else if(life < 0.75*BASE_LIFE) {
+		g = -128 + life*512.0/BASE_LIFE;
+	} else {
+		g = 255;
+	}
+	if(life < 0.5*BASE_LIFE) {
+		b = 0;
+	} else if(life < BASE_LIFE) {
+		b = -255 + life*512.0/BASE_LIFE;
+	} else {
+		b = 255;
+	}
+	float size = life < 0.1*BASE_LIFE ? life*BASE_SIZE/(0.1*BASE_LIFE): BASE_SIZE;
+	SDL_SetTextureColorMod(tex, r, g, b);
+	SDL_RenderCopy(renderer, tex, NULL, &(SDL_Rect){particle.x - size/2, particle.y - size/2, size, size});
+	return 1;
+}
+
+//Emits particles at random angles
+//pps is the nominal emission rate, in particles per second
+struct Particle particle_emit_circle(struct Particle particle, int dt, int x, int y, float pps) {
+	if(particle.life <= 0){
+		particle.life = BASE_LIFE + rand() * 500.0 / RAND_MAX;
+		particle.x = x;
+		particle.y = y;
+		float theta = rand() * 2*M_PI / RAND_MAX;
+		float v = rand() * 30.0 / RAND_MAX;
+		particle.vx = v * cos(theta);
+		particle.vy = v * sin(theta);
+	}
+	return particle;
+}
+
+//basic physics tick for particles
+struct Particle particle_tick(struct Particle particle, int dt) {
+			particle.life -= dt;
+			//Accelerate
+			particle.vy += -0.1*dt;
+			//This keeps the vx unit in pixels/second
+			particle.x += (particle.vx*dt/1000.0);
+			particle.y += (particle.vy*dt/1000.0);
+			return particle;
 }
 
 int
@@ -113,60 +166,15 @@ main(int argc, char *argv[]) {
 			}
 		}
 		dt = SDL_GetTicks() - t;
+		printf("%4.3f\n", 1.0/dt*1000.0);
 		t += dt;
 		for(int i = 0; i < PARTICLE_COUNT; i++) {
-			//Accelerate
-			particles[i].vy += -0.1*dt;
-			//This keeps the vx unit in pixels/second
-			particles[i].x += (particles[i].vx*dt/1000.0);
-			particles[i].y += (particles[i].vy*dt/1000.0);
-
-			
-			//Bring out your dead
-			particles[i].life -= dt;
-			if(particles[i].life <= 0){
-				particles[i].life = BASE_LIFE + rand() * 500.0 / RAND_MAX;
-				particles[i].x = WINDOW_WIDTH/2 - 8;
-				particles[i].y = WINDOW_HEIGHT/2 - 8 + 64;
-				float theta = rand() * 2*M_PI / RAND_MAX;
-				float v = rand() * 30.0 / RAND_MAX;
-				particles[i].vx = v * cos(theta);
-				particles[i].vy = v * sin(theta);
-			}
-
+			particles[i] = particle_tick(particles[i], dt);
+			particles[i] = particle_emit_circle(particles[i], dt, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 100);
+			particle_draw(particles[i], back_sprite, renderer);
 		}
-
 		for(int i = 0; i < PARTICLE_COUNT; i++) {
-			draw1(particles[i], back_sprite, renderer);
-		}
-
-		for(int i = 0; i < PARTICLE_COUNT; i++) {
-			int life = particles[i].life;
-			int r, g, b;
-			r = life > BASE_LIFE? 255 : life*(255/BASE_LIFE);
-			if(life < 0.25*BASE_LIFE) {
-				r = life*255/(0.25*BASE_LIFE);
-			} else {
-				r = 255;
-			}
-			if(life < 0.25*BASE_LIFE) {
-				g = 0;
-			} else if(life < 0.75*BASE_LIFE) {
-				g = -128 + life*512.0/BASE_LIFE;
-			} else {
-				g = 255;
-			}
-			if(life < 0.5*BASE_LIFE) {
-				b = 0;
-			} else if(life < BASE_LIFE) {
-				b = -255 + life*512.0/BASE_LIFE;
-			} else {
-				b = 255;
-			}
-			float size = life < 0.1*BASE_LIFE ? life*BASE_SIZE/(0.1*BASE_LIFE): BASE_SIZE;
-			//filledCircleRGBA(renderer, particles[i].x, particles[i].y, 4, r, g, b, 255);
-			 SDL_SetTextureColorMod(particle_sprite, r, g, b);
-			 SDL_RenderCopy(renderer, particle_sprite, NULL, &(SDL_Rect){particles[i].x - size/2, particles[i].y - size/2, size, size});
+			draw2(particles[i], particle_sprite, renderer);
 		}
 
 		SDL_RenderPresent(renderer);
